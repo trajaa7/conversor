7317a0ec360675aa286d988043b45ac69144cf06
Giovanalupo <130668804+Giovanalupo@users.noreply.github.com>
2023-10-23 08:49:46 -0300

    conversor commit

---
+++ conversor.py
@@ -0,0 +1,7 @@
+def celsius_para_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def fahrenheit_para_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
---
+++ main.py
@@ -0,0 +1,24 @@
+
+from conversor import celsius_para_fahrenheit, fahrenheit_para_celsius
+
+while True:
+        print('\n\t\t • Conversor • \n\t\t')
+
+        print('Escolha uma opção:')
+        print('1. Converter de Celsius para Fahrenheit')
+        print('2. Converter de Fahrenheit para Celsius')
+        print('3. Sair \n')
+
+        opcao = input('Digite o número da opção desejada: ')
+        if opcao == '1':
+            celsius = float(input('Digite a temperatura em Celsius: '))
+            fahrenheit = celsius_para_fahrenheit(celsius)
+            print(f"{celsius}ºC é igual a {fahrenheit}ºF")
+        elif opcao == '2':
+            fahrenheit = float(input('Digite a temperatura em Fahrenheit: '))
+            celsius = fahrenheit_para_celsius(fahrenheit)
+            print(f'{fahrenheit}ºF é igual a {celsius}ºC')
+        elif opcao == '3':
+            break
+        else:
+            print('Opção inválida. Tente novamente.')
